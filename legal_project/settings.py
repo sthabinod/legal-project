f22e01ffe468a8aff86e4e089002d2bc9c8a5c91
kushtech2078 <info.kushtechnp@gmail.com>
2024-02-11 19:08:13 +0545

    sidebar commit

--- legal_project/settings.py
+++ legal_project/settings.py
@@ -81,23 +81,23 @@ from dotenv import load_dotenv
 load_dotenv()
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME':"legal_db",
-#         'USER': "legal_user",
-#         'PASSWORD': "legal_password",
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':"legal_db",
+        'USER': "legal_user",
+        'PASSWORD': "legal_password",
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
